b0437df072f5678f9f60ed898305f0a62eb20820
Prashant Kiran <prasht63@gmail.com>
2016-04-06 19:21:03 +0530

    Update db_config.php

--- resources/db_config.php
+++ resources/db_config.php
@@ -1,8 +1,8 @@
 <?php
 ini_set('display_errors', '1');
 $host = "localhost";
-$user = "root";
-$password = "root";
+$user = "stash_admin";
+$password = "1q2w3e4rA";
 $db = "stage_shastra";
 $con=mysqli_connect($host,$user,$password,$db);
 if ( mysqli_connect_errno() ) {
@@ -10,5 +10,4 @@ if ( mysqli_connect_errno() ) {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
             echo "</div>";
         }
-
 ?>
