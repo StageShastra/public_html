7c8966646b164dc091ee73b4ee8096825b05d102
root <root@ip-172-31-21-180.ap-southeast-1.compute.internal>
2016-05-29 08:09:37 +0000

    Setting config

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost:8888/stash/public_html';
+$config['base_url'] = 'http://castiko.com/';
 
 
 /*
