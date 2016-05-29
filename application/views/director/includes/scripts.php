0b4ec0710d4e9247974a8b5461ddaf6322a760a6
Prashant Kiran <prasht63@gmail.com>
2016-05-29 12:36:21 +0530

    Removed Google Analytics Code

--- application/views/director/includes/scripts.php
+++ application/views/director/includes/scripts.php
@@ -7,15 +7,6 @@
         <script src="<?= JS ?>/lightbox.js"></script>
         <script src="<?= JS ?>/stupidtable.js"></script>
         <!-- Google Analytics: change UA-XXXXX-X to  be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
-        
         <footer class="footer">
           <foodiv class="container center">
             <p class="dark-gray info-small center "><?= M_Copyright ?></p>
