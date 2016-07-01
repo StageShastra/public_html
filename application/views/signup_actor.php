a4fc438b5b032dc4d51c7f9e3d83f058af43210a
Prashant Kiran <prasht63@gmail.com>
2016-07-01 21:26:47 +0530

    removed burger menu

--- application/views/signup_actor.php
+++ application/views/signup_actor.php
@@ -138,12 +138,7 @@ a:hover, a:focus {
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+                
                 <a class="navbar-brand" href="<?= base_url() ?>">
                             <img src="<?= IMG ?>/logo.png" class="brands img-responsive "/>
                             <div class="vertical-middle brandname title">
