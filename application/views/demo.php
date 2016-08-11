9f9409c7ce0df008bb3b2d10b857e9a04f11742a
Prashant Kiran <prasht63@gmail.com>
2016-08-11 20:59:35 +0530

    Demo page

---
+++ application/views/demo.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Castiko | Register as an Casting Director</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Propel your casting business forward - sign up to create a Castiko account to manage your data, run auditions and find new actors">
+        <meta name="keywords" content="Castiko, Acting, Audition, Actor, Casting, Sign Up">
+        <meta name="filename" content="Castiko-Actor-Casting-Director-Pricing">
+        <meta property="og:title" content="Castiko | Register as an Casting Director"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="http://www.castiko.com/"/>
+        <meta property="og:image" content="http://www.castiko.com/assets/img/logo.png"/>
+        <meta property="og:description" content="Propel your casting business forward - sign up to create a Castiko account to manage your data, run auditions and find new actors"/>
+        <link rel="shortcut icon" href="<?= IMG ?>/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="<?= IMG ?>/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="<?= IMG ?>/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="<?= CSS ?>/bootstrap.min.css">
+        <link rel="stylesheet" href="<?= CSS ?>/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="<?= CSS ?>/main.css">
+        <link rel="stylesheet" href="<?= CSS ?>/navbar.css">
+        <link rel="stylesheet" href="<?= CSS ?>/font-awesome.css">
+        <link href='https://fonts.googleapis.com/css?family=Raleway:700,800,400,200,300,100' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+        <script type="text/javascript" src="http://code.jquery.com/jquery-2.0.0.js"></script>
+        <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.js"></script>
+    
+      <script type="text/javascript">
+    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+    heap.load("408837571");
+  </script>
+
+    <script>
+      var registerSuccess = false;
+    </script>
+
+      
+    </head>
+<link href="<?= CSS ?>/navbar.css" rel="stylesheet" type="text/css">
+<style>
+body{
+  padding-top: 120px;
+  background-color: rgb(240, 87, 89);
+  background-image: linear-gradient(-228deg, #F05759 0%, rgb(240, 87, 89) 31%, rgba(240,87,89,0.97) 78%, #F05759 95%);
+  padding-bottom: 0px;
+}
+.submit-btn {
+    background-color:#FBB515; ;
+    border-color: #E19B1B;
+    margin-top: 10px;
+    padding: 5px 10px;
+    color: white;
+}
+.submit-btn:hover, .submit-btn:focus, .submit-btn:active{
+  background-color: white;
+  color: #e19b1b;
+}
+.submit-btn{
+  padding: 5px 10px;
+  margin-bottom: 20px;
+}
+.signup_heading{
+    font-family: Raleway;
+    font-size: 24px;
+    color: black;
+}
+@media(max-width:767px) {
+  body{
+    background: white;
+
+  }
+.signup_box{
+  background-image: linear-gradient(29deg, rgb(226, 162, 17) 3%, rgba(216, 115, 27, 0.86) 20%, rgba(195, 15, 76, 0.86) 100%, rgb(255, 0, 35) 10%);
+  border-radius: 10px;
+}
+.signup_heading{
+  font-family: Raleway;
+  font-size: 21px;
+  color: #fff;
+}
+.already{
+  font-family: "Open Sans","Raleway";
+  color: white;
+}
+.submit-btn {
+    background-color:white ;
+    border-color: #E19B1B;
+    margin-top: 10px;
+    padding: 5px 10px;
+    color: #E19B1B;
+}
+.submit-btn:hover, .submit-btn:focus, .submit-btn:active{
+  background-color: #FBB515;
+  color: white;
+  border-color: #E19B1B;
+}
+#success{
+  color: white;
+}
+    
+}
+.signup_content{
+  background: white;
+  border-radius: 15px;
+  margin-bottom: 20px; 
+}
+#form-div{
+  padding-bottom: 20px;
+}
+a:hover, a:focus {
+    color: #23527c;
+    text-decoration: none;
+}
+.title{
+  color: black;
+}
+.footer-items a {
+    color: rgba(255,255,255,0.7);
+}
+.footer-items a:hover {
+    color: rgba(255,255,255,1);
+}
+
+</style>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div class="container signup_content">
+          <div class="container-fluid center col-lg-12 col-sm-10 col-xs-12"> <!--container fluid starts -->
+             <nav class="navbar navbar-custom navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <a class="navbar-brand" href="<?= base_url() ?>">
+                            <img src="<?= IMG ?>/logo.png" class="brands img-responsive "/>
+                            <div class="vertical-middle brandname title">
+                                <?= M_Title ?>
+                                <br>
+                                <span id="tag-line" class="firstcolor info-small hidden-xs">
+                                Making Casting easier!                      
+                                </span>
+                            </div>
+                            
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+            <div class="row center signup_box">
+              <div class="col-sm-12 light-padded">
+                <div class="col-lg-6 col-md-6 col-sm-6 hidden-xs" style="margin-top:45px;">
+                  <img src="<?= IMG ?>/macbook_cd_register.png" class="animation-element slide-left img-responsive laptoponly" >
+                </div>
+                <div class="col-lg-6 col-sm-6 col-xs-12" id="form-div">
+
+                  <font class="info-small text-primary signup_heading">Demo Request</font>
+                  
+                      <form  id="demoForm" >
+                      <div class="form-group">
+                        <input type="text" class="form-control login" id="name_requester" name="name" placeholder= "Full Name *" required>
+                      </div>
+                      <div class="form-group">
+                        <input type="text" class="form-control login" id="contact_requester" name="email" placeholder= "Phone/Email *" required >
+                      </div>
+                      <div class="form-group">
+                        <input type="text" class="form-control login" id="date_requested" name="date" placeholder= "What date are you free?*" required >
+                      </div>
+                     
+                      <!-- <div class='form-group'>
+                        I am <input type="radio" name="type" value="director" checked> Director
+                              <input type="radio" name="type" value="actor"> Actor
+                      </div> -->
+                      <button type="button" id="submit_demo_request" class="btn btn-custom-outlined submit-btn"><span class="glyphicon glyphicon-log-in"></span> &nbsp; Request Demo</button>
+                      <center><a href="<?= base_url() ?>" class="already"><small><?= M_AlreadyRegistered ?></small></a></center>
+                    </form>
+                
+					
+					<?php echo validation_errors("<p class='text-danger'>"); ?>
+
+					
+                
+					
+				</div>
+				
+				
+				
+                 <div class="container-fluid hidden" id="success">
+                  <div class="col-sm-6 col-lg-6 col-xs-12 col-md-6">
+
+                    <!-- <img src="<?= IMG ?>/thanks.png"  style="float:left; height:50%;"/> -->
+					<h1 style="margin-top:15%;color:white;" class=""> Thank You!</b></h1><br>
+					
+					
+
+                  </div>
+                </div>  
+
+              </div>
+              </div>
+            </div>
+          </div>
+          </div>
+          <br><br><br><br><br>
+        </div>
+<?php
+  include "includes/footer.php";
+  include "includes/scripts.php";
+?>
